49ee9cfc48ee6922e1fbc448cf87ca6b5badd990
GREY FIVE NINE <greyfivenine@TRASH.local>
2026-02-19 09:17:19 -0800

    Move Firebase keys to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,10 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8PriJptshsg2QCfWtZgH9D15fCalz38A",
-  authDomain: "art-vault-85395.firebaseapp.com",
-  projectId: "art-vault-85395",
-  storageBucket: "art-vault-85395.firebasestorage.app",
-  messagingSenderId: "717114829074",
-  appId: "1:717114829074:web:12f29355dee5048943abcb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
