0d6b54ab07623ea66a208c50502d7133c090a9e0
GREY FIVE NINE <greyfivenine@TRASH.local>
2026-02-28 22:51:31 -0800

    Temporarily hardcode Firebase config for testing

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIza5yB8PrIjptshsg2QCtWtZgH9D15fCalz38A",
+  authDomain: "art-vault-85395.firebaseapp.com",
+  projectId: "art-vault-85395",
+  storageBucket: "art-vault-85395.firebasestorage.app",
+  messagingSenderId: "717114829074",
+  appId: "1:717114829074:web:12f29355dee5048943abcb"
 };
 
 const app = initializeApp(firebaseConfig);
